30acf96ae4d03b20547f6e601e216d7235240c38
Luiz Carlos <developer.luizcarlos@outlook.com>
2024-08-22 19:07:45 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const server = express();
+const server: Express = express();
 
-server.get("/", (req, res) => {
-    return res.send("Olá, Developer!");
+server.get("/", (req: Request, res: Response) => {
+  return res.send("Olá, Developer!");
 });
 
 export { server };
